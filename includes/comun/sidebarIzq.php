bd94e1709955cb93e9b9896964de927545cf8752
fer2807 <frg1998@gmail.com>
2020-05-19 20:20:59 +0200

    cartelera ya redirecciona a cartelera.php y no a contenido.php

--- includes/comun/sidebarIzq.php
+++ includes/comun/sidebarIzq.php
@@ -1,7 +1,7 @@
 <div id="sidebar-left">
 	<ul>
 		<li><a href="index.php">INICIO</a></li>
-		<li><a href="contenido.php">CARTELERA</a></li>		
+		<li><a href="cartelera.php">CARTELERA</a></li>		
 		<li><a href="companies.php">COMPAÑIAS</a></li>
 		<li><a href="admin.php">ADMINISTRAR</a></li>
 	</ul>
