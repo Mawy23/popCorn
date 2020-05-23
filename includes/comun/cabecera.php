942407fa18c5ee7b537554ac67740f2831d220a5
fer2807 <frg1998@gmail.com>
2020-05-23 13:06:18 +0200

    css mejorando

--- includes/comun/cabecera.php
+++ includes/comun/cabecera.php
@@ -7,15 +7,30 @@
 	            <p><a href="index.php">PopCorn&Chill</a></p>
 	    </div>
 	</div>
-	<div class="saludo">
-	<?php
-	if (isset($_SESSION["login"]) && ($_SESSION["login"]===true)) {
-		echo "Bienvenido, " . $_SESSION['nombre'] . ".<a href='logout.php'>(salir)</a>";
+	<div class = "menu">
+	<ul class = "align-left">
+		<a href="index.php">INICIO</a>
+		<a href="cartelera.php">CARTELERA</a>		
+		<a href="companies.php">COMPAÑIAS</a>
+		<a href="admin.php">ADMINISTRAR</a> 	
+	</ul>
+	
+	<div class = "align-right" >
+		<ul class = "saludo-der">
+		<?php
+		if (isset($_SESSION["login"]) && ($_SESSION["login"]===true)) {
+			
+			echo " Bienvenido, " . $_SESSION['nombre'] . ".<a href='logout.php'>(salir)</a>";
+			
+		} else {
+			echo "<a href='login.php'>Login</a> <a href='registro.php'>Registro</a>";
+		}
+		?></ul></div>
 		
-	} else {
-		echo "<a href='login.php'>Login</a> <a href='registro.php'>Registro</a>";
-	}
-	?>
 	</div>
+	
+	
+		
+		
 </div>
 
